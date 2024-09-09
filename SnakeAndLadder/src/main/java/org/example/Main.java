8aa8c11523400f5c3d393db811424ebdbeecd9f8
Shrikar Kulkarni <shrikar2112@gmail.com>
2024-09-09 13:37:20 +0530

    Update Main.java

--- SnakeAndLadder/src/main/java/org/example/Main.java
+++ SnakeAndLadder/src/main/java/org/example/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         List<Shifter> snakes = List.of(new Snake(98, 79), new Snake(95, 75), new Snake(87, 36), new Snake(62, 19));
         List<Shifter> ladders = List.of(new Ladder(28, 84), new Ladder(80, 99), new Ladder(9, 31), new Ladder(4, 14));
-        List<Player> players = List.of(new Player("shrikar"), new Player("keerthi"), new Player("shriyash"));
+        List<Player> players = List.of(new Player("Yudhistir"), new Player("Bhim"), new Player("Arjun"),new Player("Nakul"));
         RandomStrategy dice = new SixFaceDice();
         Board board = new Board(snakes, ladders, players, dice);
         board.start();
